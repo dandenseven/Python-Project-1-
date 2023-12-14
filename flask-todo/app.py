0ca90466649a72618f05163da942bfaf03d9e0b4
Dane knight <knight1seven@gmail.com>
2023-12-13 19:45:34 -0500

    app route update and delete

--- flask-todo/app.py
+++ flask-todo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,12 +13,37 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-@app.route('/')
+@app.route("/")
 def index():
     # show all todos
     todo_list = Todo.query.all()
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    # add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # update item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # delete item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 
 
